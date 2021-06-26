41b86f4271648a1beef070bd1f098a74d1b21971
spreeRaj <mithiya.raj@gmail.com>
2021-06-26 18:54:00 +0100

    client menu validation

--- connectfiveclient/src/main/java/com/game/connect/five/App.java
+++ connectfiveclient/src/main/java/com/game/connect/five/App.java
@@ -23,26 +23,31 @@ public final class App {
 
         // Setting up client input
         Scanner sc = new Scanner(System.in);
-
         int choice = 0;
-        System.out.println();
         do {
 
-            System.out.println("1. Start a new game");
-            System.out.println("2. Join current game");
-            System.out.println("3. Exit game");
-            System.out.print("Enter choice: ");
-            choice = sc.nextInt();
-
+            try {
+                System.out.println();
+                System.out.println("1. Start a new game");
+                System.out.println("2. Join current game");
+                System.out.println("3. Exit game");
+                System.out.print("Enter choice: ");
+                choice = sc.nextInt();
+            } catch (Exception e) {
+                System.out.println("Invalid Input !! Enter integer");
+                continue;
+            }
             if (choice == 3) {
 
                 System.out.println("*********************************");
                 System.out.println("*           Thank you !!        *");
                 System.out.println("*        See you Later!! :)     *");
                 System.out.println("*********************************");
+                sc.close();
                 System.exit(0);
 
             }
+
         } while (true);
 
     }
